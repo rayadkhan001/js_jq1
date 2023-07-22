d95aade1b3f2252d6713b195fb41d97a1f8029ca
Rayad Hassin Khan Rahin <129431179+rayadkhan001@users.noreply.github.com>
2023-07-23 05:26:46 +0600

    js_jq1
    js_jq1

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// external js
+// text change
+// show date
+// variable repeat
+function dataRepeat(){
+	var massage = "Welcome to SS";
+	document.getElementById('dr').innerHTML = massage.repeat(3);
+}
+// on & off
+function bulbOn(){
+	document.getElementById('bulb').src = 'img/on.gif';
+}
+function bulbOff(){
+	document.getElementById('bulb').src = 'img/off.gif';
+}
+// fontsize
+function fontsize(){
+	document.getElementById('fs').style.fontSize = "50px";
+}
+// show/hide
+// javaScript Numbers
+function demo2(){
+    document.getElementById("demo2").innerHTML = 10.50;
+}
+// JavaScript Comments 1st
+// Change heading:
+    document.getElementById("myH").innerHTML = "JavaScript Comments";
+// Change paragraph:
+    document.getElementById("myP").innerHTML = "My first paragraph.";
+
